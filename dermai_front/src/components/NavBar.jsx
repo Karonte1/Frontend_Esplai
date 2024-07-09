6e96bdb26ebe8144e694cf9580d8c1af7383dd40
Roberta Giuliani <rg.giuliani4@gmail.com>
2024-07-09 16:13:38 +0200

    add base project

---
+++ dermai_front/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div>
+      <ul className="nav_menu">
+        <li>Home</li>
+        <li>XXX</li>
+        <li>XXX</li>
+        <li>XXX</li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
